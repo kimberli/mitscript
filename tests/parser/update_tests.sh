e49691bb38e591a94597185ff9a2e1025da683ed
kimberli <kimberlizhong@gmail.com>
2018-02-20 18:33:18 -0500

    update custom tests

---
+++ tests/parser/update_tests.sh
@@ -0,0 +1,10 @@
+cat input/test1.mit | ../../parser/mitscript-parser > output/test1.mit.out
+cat input/test2.mit | ../../parser/mitscript-parser > output/test2.mit.out
+cat input/test3.mit | ../../parser/mitscript-parser > output/test3.mit.out
+
+cat input/bad6.mit | ../../parser/mitscript-parser > output/bad6.mit.out
+cat input/bad7.mit | ../../parser/mitscript-parser > output/bad7.mit.out
+cat input/bad8.mit | ../../parser/mitscript-parser > output/bad8.mit.out
+cat input/bad9.mit | ../../parser/mitscript-parser > output/bad9.mit.out
+cat input/bad10.mit | ../../parser/mitscript-parser > output/bad10.mit.out
+cat input/bad11.mit | ../../parser/mitscript-parser > output/bad11.mit.out
