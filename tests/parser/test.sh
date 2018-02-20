4028aad1b88585f6fde63736d421a4d11bab62ad
kimberli <kimberlizhong@gmail.com>
2018-02-20 18:53:00 -0500

    prettier testing

--- tests/parser/test.sh
+++ tests/parser/test.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 ROOT=$(git rev-parse --show-toplevel)
 PARSER=$ROOT/parser/mitscript-parser
+TOTAL=0
+SUCCESS=0
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 for filename in $ROOT/tests/parser/input/*; do
-    echo "Test - $(basename $filename)"
+    echo "==== TEST - $(basename $filename) ===="
     cat $filename | $PARSER > tmp.out
+    TOTAL=$((TOTAL+1))
     if diff tmp.out $ROOT/tests/parser/output/$(basename $filename).out; then
-        echo "Test Passed"
+        SUCCESS=$((SUCCESS+1))
+        echo -e "${GREEN}Test Passed${NC}"
     else
-        echo "Test Failed"
+        echo -e "${RED}Test Failed${NC}"
     fi
     echo ""
     rm tmp.out
 done
+
+echo "Finished tests - ${SUCCESS}/${TOTAL} passed"
