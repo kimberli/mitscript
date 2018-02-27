867e445086141b2701194480341f826271c45874
Student <dvargas92495@gmail.com>
2018-02-27 12:05:05 -0500

    Fixing up Interpreter tests and added test.sh

---
+++ tests/interpreter/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ROOT=$(git rev-parse --show-toplevel)
+PARSER=$ROOT/parser/mitscript
+
+for filename in $ROOT/tests/interpreter/bad*.mit; do
+    echo "Test - $(basename $filename)"
+    if $PARSER $filename > tmp.out; then
+        echo "Test Failed"
+    else
+        echo "Test Passed"
+    fi
+    echo ""
+    rm tmp.out
+done
+
+for filename in $ROOT/tests/interpreter/good*.mit; do
+    echo "Test - $(basename $filename)"
+    $PARSER $filename > tmp.out
+    if diff tmp.out $ROOT/tests/interpreter/$(basename $filename .mit).output; then
+        echo "Test Passed"
+    else
+        echo "Test Failed"
+    fi
+    echo ""
+    rm tmp.out
+done
