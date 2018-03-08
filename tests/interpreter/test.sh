47945322954f48df446bf75541f20c7621bcc16a
kimberli <kimberlizhong@gmail.com>
2018-03-07 22:36:23 -0500

    prettier testing

--- tests/interpreter/test.sh
+++ tests/interpreter/test.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 ROOT=$(git rev-parse --show-toplevel)
 PARSER=$ROOT/parser/mitscript
+TOTAL=0
+SUCCESS=0
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 for filename in $ROOT/tests/interpreter/bad*.mit; do
-    echo "Test - $(basename $filename)"
+    echo "==== TEST - $(basename $filename) ===="
+    TOTAL=$((TOTAL+1))
     if $PARSER $filename > tmp.out; then
-        echo "Test Failed"
+        echo -e "${RED}Test Failed${NC}"
     else
-        echo "Test Passed"
+        SUCCESS=$((SUCCESS+1))
+        echo -e "${GREEN}Test Passed${NC}"
     fi
     echo ""
     rm tmp.out
 done
 
 for filename in $ROOT/tests/interpreter/good*.mit; do
-    echo "Test - $(basename $filename)"
+    echo "==== TEST - $(basename $filename) ===="
+    TOTAL=$((TOTAL+1))
     $PARSER $filename > tmp.out
     if diff tmp.out $ROOT/tests/interpreter/$(basename $filename .mit).output; then
-        echo "Test Passed"
+        SUCCESS=$((SUCCESS+1))
+        echo -e "${GREEN}Test Passed${NC}"
     else
-        echo "Test Failed"
+        echo -e "${RED}Test Failed${NC}"
     fi
     echo ""
     rm tmp.out
 done
+
+echo "Finished tests - ${SUCCESS}/${TOTAL} passed"
