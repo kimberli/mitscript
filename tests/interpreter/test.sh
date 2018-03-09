26ffa095fa034d8677880b97a6864fdfad8935ae
kimberli <kimberlizhong@gmail.com>
2018-03-09 00:38:12 -0500

    reorg tests

--- tests/interpreter/test.sh
+++ tests/interpreter/test.sh
@@ -7,20 +7,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-for filename in $ROOT/tests/interpreter/bad*.mit; do
-    echo "==== TEST - $(basename $filename) ===="
-    TOTAL=$((TOTAL+1))
-    if $PARSER $filename > tmp.out; then
-        echo -e "${RED}Test Failed${NC}"
-    else
-        SUCCESS=$((SUCCESS+1))
-        echo -e "${GREEN}Test Passed${NC}"
-    fi
-    echo ""
-    rm tmp.out
-done
-
-for filename in $ROOT/tests/interpreter/good*.mit; do
+for filename in $ROOT/tests/interpreter/*.mit; do
     echo "==== TEST - $(basename $filename) ===="
     TOTAL=$((TOTAL+1))
     $PARSER $filename > tmp.out
