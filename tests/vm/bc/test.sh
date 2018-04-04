1c1317f15a4cc2a5b17af42b7b8e168e016cafce
kimberli <kimberlizhong@gmail.com>
2018-04-03 21:26:04 -0400

    reorganize tests

--- tests/vm/bc/test.sh
+++ tests/vm/bc/test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 ROOT=$(git rev-parse --show-toplevel)
-PARSER=$ROOT/vm/interpreter
+PROG=$ROOT/vm/interpreter
 TOTAL=0
 SUCCESS=0
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-for filename in $ROOT/tests/vm/*.mit; do
+for filename in $ROOT/tests/vm/bc/*.mitbc; do
     echo "==== TEST - $(basename $filename) ===="
     TOTAL=$((TOTAL+1))
-    $PARSER $filename > tmp.out
-    if diff tmp.out $ROOT/tests/vm/$(basename $filename .mit).output; then
+    $PROG -b $filename > tmp.out
+    if diff tmp.out $ROOT/tests/vm/bc/$(basename $filename .mitbc).output; then
         SUCCESS=$((SUCCESS+1))
         echo -e "${GREEN}Test Passed${NC}"
     else
