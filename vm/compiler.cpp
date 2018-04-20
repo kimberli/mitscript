bf4ef9f626d3134460b7abbad39c397157794922
Stef Ren <stefren@mit.edu>
2018-04-20 08:04:45 -0400

    stripping whitespace

--- vm/compiler.cpp
+++ vm/compiler.cpp
@@ -50,7 +50,7 @@ void BytecodeCompiler::addWriteInstructions(Expression* lhs) {
     if (fieldD != NULL) {
         // load the record
         addInstructions(fieldD->base);
-        // allocate a name 
+        // allocate a name
         int i = allocName(fieldD->field.name);
         // we need to swap the order: we want s :: record :: value
         Instruction* swapInstr = new Instruction(Operation::Swap, optint_t());
@@ -64,7 +64,7 @@ void BytecodeCompiler::addWriteInstructions(Expression* lhs) {
     if (indexE != NULL) {
         Instruction* swap = new Instruction(Operation::Swap, optint_t());
         // we need S :: record :: index :: value, so we need 2 swaps
-        // load the record 
+        // load the record
         addInstructions(indexE->base);
         retFunc->instructions.push_back(*swap);
         // load the index
@@ -101,8 +101,8 @@ void BytecodeCompiler::addWriteVarInstructions(std::string varName) {
 }
 
 void BytecodeCompiler::loadBuiltIns() {
-    // for each func, create a func /w right amount of args, 
-    // add to the curent frame, 
+    // for each func, create a func /w right amount of args,
+    // add to the curent frame,
     // then generate code to load globally
     funcptr_t printFunc = new Function();
     printFunc->parameter_count_ = 1;
@@ -113,8 +113,8 @@ void BytecodeCompiler::loadBuiltIns() {
     retFunc->instructions.push_back(*loadPrint);
     retFunc->instructions.push_back(*allocPrint);
     addWriteVarInstructions("print");
-    
-    // input 
+
+    // input
     funcptr_t inputFunc = new Function();
     inputFunc->parameter_count_ = 0;
     int inputIdx = retFunc->functions_.size();
@@ -138,11 +138,11 @@ void BytecodeCompiler::loadBuiltIns() {
 }
 
 funcptr_t BytecodeCompiler::evaluate(Expression& exp) {
-    // set current function 
+    // set current function
     retFunc = new Function();
     retFunc->parameter_count_ = 0;
 
-    // generate a symbol table 
+    // generate a symbol table
     SymbolTableBuilder stb = SymbolTableBuilder();
     symbolTables = stb.eval(exp);
     curTable = symbolTables.at(0);
@@ -152,11 +152,11 @@ funcptr_t BytecodeCompiler::evaluate(Expression& exp) {
         std::string varName = it->first;
         desc_t d = it->second;
         switch (d->type) {
-            case GLOBAL: 
+            case GLOBAL:
                 d->index = retFunc->names_.size();
                 retFunc->names_.push_back(varName);
                 break;
-            case LOCAL: 
+            case LOCAL:
                 d->index = retFunc->local_vars_.size();
                 retFunc->local_vars_.push_back(varName);
                 if (d->isReferenced) {
@@ -164,9 +164,9 @@ funcptr_t BytecodeCompiler::evaluate(Expression& exp) {
                     retFunc->local_reference_vars_.push_back(varName);
                 }
                 break;
-            case FREE: 
+            case FREE:
                 d->index = retFunc->free_vars_.size();
-                retFunc->free_vars_.push_back(varName); 
+                retFunc->free_vars_.push_back(varName);
         }
     }
 
@@ -176,7 +176,7 @@ funcptr_t BytecodeCompiler::evaluate(Expression& exp) {
     // run this visitor
     exp.accept(*this);
 
-    // return the function. 
+    // return the function.
     return retFunc;
 
 }
@@ -236,21 +236,21 @@ void BytecodeCompiler::visit(IfStatement& exp) {
 void BytecodeCompiler::visit(WhileLoop& exp) {
     // add instructions for evaluating the condition
     int startsize = retFunc->instructions.size();
-     
-    // add the body 
+
+    // add the body
     addInstructions(exp.body);
     int endBody = retFunc->instructions.size();
 
-    // insert the goto to skip to the condition 
+    // insert the goto to skip to the condition
     int bodySize = endBody - startsize;
-    InstructionList::iterator startPos = retFunc->instructions.begin() + startsize; 
+    InstructionList::iterator startPos = retFunc->instructions.begin() + startsize;
     Instruction* goInstr = new Instruction(Operation::Goto, optint_t(bodySize + 1));
     retFunc->instructions.insert(startPos, *goInstr);
 
-     // add the condition 
+     // add the condition
     addInstructions(exp.condition);
     int endCondition = retFunc->instructions.size();
-    
+
     int conditionAndBodySize = endCondition-startsize-1; // -1 for the goto
     // add the if which takes you back to the start of the body
     Instruction* ifInstr = new Instruction(Operation::If, optint_t(-conditionAndBodySize));
@@ -266,11 +266,11 @@ void BytecodeCompiler::visit(Return& exp) {
 void BytecodeCompiler::putVarInFunc(std::string& varName, stptr_t table, funcptr_t func) {
     desc_t d = table->vars.at(varName);
     switch (d->type) {
-        case GLOBAL: 
+        case GLOBAL:
             d->index = func->names_.size();
             func->names_.push_back(varName);
             break;
-        case LOCAL: 
+        case LOCAL:
             d->index = func->local_vars_.size();
             func->local_vars_.push_back(varName);
             if (d->isReferenced) {
@@ -278,24 +278,24 @@ void BytecodeCompiler::putVarInFunc(std::string& varName, stptr_t table, funcptr
                 func->local_reference_vars_.push_back(varName);
             }
             break;
-        case FREE: 
+        case FREE:
             d->index = func->free_vars_.size();
-            func->free_vars_.push_back(varName); 
+            func->free_vars_.push_back(varName);
             break;
     }
 }
 
 void BytecodeCompiler::visit(FunctionExpr& exp) {
     // 1) get the corresponding symbol table
-    stCounter += 1; 
+    stCounter += 1;
     stptr_t childTable = symbolTables.at(stCounter);
 
-    // 2) make the new function object 
+    // 2) make the new function object
     funcptr_t childFunc = new Function();
     childFunc->parameter_count_ = exp.args.size();
 
     // load up childFunc with vars from symbol table
-    // start with args in order 
+    // start with args in order
     std::set<std::string> argNames;
     for (Identifier* arg : exp.args) {
         std::string argName = arg->name;
@@ -313,7 +313,7 @@ void BytecodeCompiler::visit(FunctionExpr& exp) {
 
     // 3) install the child's state and run
     funcptr_t parentFunc = retFunc;
-    retFunc = childFunc; 
+    retFunc = childFunc;
     stptr_t parentTable = curTable;
     curTable = childTable;
 
@@ -329,7 +329,7 @@ void BytecodeCompiler::visit(FunctionExpr& exp) {
     retFunc = parentFunc;
     curTable = parentTable;
 
-    // 4) add the function to the function list with an index. 
+    // 4) add the function to the function list with an index.
     int childFuncIdx = retFunc->functions_.size();
     retFunc->functions_.push_back(childFunc);
 
@@ -337,18 +337,18 @@ void BytecodeCompiler::visit(FunctionExpr& exp) {
     Instruction* loadF = new Instruction(Operation::LoadFunc, optint_t(childFuncIdx));
     retFunc->instructions.push_back(*loadF);
 
-    // 6) load refs to all the child's free vars. 
+    // 6) load refs to all the child's free vars.
     // THESE NEED TO GO ON BACKWARDS
     //for (std::string var : childFunc->free_vars_) {
     vector<string> freeVars = childFunc->free_vars_;
     for (vector<string>::reverse_iterator it = freeVars.rbegin();
-         it != freeVars.rend(); 
+         it != freeVars.rend();
          it++) {
         std::string var = *it;
         // get that var's description in the parent
         desc_t d = curTable->vars.at(var);
         int i;
-        // can push a ref to a local ref var or a free var 
+        // can push a ref to a local ref var or a free var
         if (d->type==LOCAL && d->isReferenced) {
             // instructions for ref variables
             i = d->refIndex;
@@ -356,14 +356,14 @@ void BytecodeCompiler::visit(FunctionExpr& exp) {
             // instructions for free variables
             i = d->index + retFunc->local_reference_vars_.size();
         } else {
-            // this is an error, there is probably a bug. 
+            // this is an error, there is probably a bug.
             assert(false);
         }
         Instruction* push = new Instruction(Operation::PushReference, optint_t(i));
         retFunc->instructions.push_back(*push);
     }
-    
-    // 7) allocate the closure 
+
+    // 7) allocate the closure
     int numRefs = childFunc->free_vars_.size();
     Instruction* allocC = new Instruction(Operation::AllocClosure, optint_t(numRefs));
     retFunc->instructions.push_back(*allocC);
@@ -373,43 +373,43 @@ void BytecodeCompiler::visit(BinaryExpr& exp) {
     addInstructions(exp.left);
     addInstructions(exp.right);
     // concatenate two vecs
-    Operation op; 
+    Operation op;
     optint_t noArg0;
     Instruction* swapOp = new Instruction(Operation::Swap, noArg0);
     // choose the correct instruction
     switch (exp.op) {
-        case Or: 
+        case Or:
             op = Operation::Or;
             break;
-        case And: 
+        case And:
             op = Operation::And;
             break;
-        case Lt: 
+        case Lt:
             // no lt instr provided, so first switch op order.
             retFunc->instructions.push_back(*swapOp);
             op = Operation::Gt;
             break;
-        case Gt: 
+        case Gt:
             op = Operation::Gt;
             break;
-        case Lt_eq: 
+        case Lt_eq:
             // same logic as for lt
             retFunc->instructions.push_back(*swapOp);
             op = Operation::Geq;
             break;
-        case Gt_eq: 
+        case Gt_eq:
             op = Operation::Geq;
             break;
-        case Eq_eq: 
+        case Eq_eq:
             op = Operation::Eq;
             break;
-        case Plus: 
+        case Plus:
             op = Operation::Add;
             break;
         case Minus:
             op = Operation::Sub;
             break;
-        case Times: 
+        case Times:
             op = Operation::Mul;
             break;
         case Divide:
@@ -426,10 +426,10 @@ void BytecodeCompiler::visit(UnaryExpr& exp) {
     optint_t noArg0;
     // choose the correct instruction
     switch (exp.op) {
-        case Not: 
+        case Not:
             op = Operation::Not;
             break;
-        case Neg: 
+        case Neg:
             op = Operation::Neg;
             break;
     }
@@ -438,19 +438,19 @@ void BytecodeCompiler::visit(UnaryExpr& exp) {
 }
 
 void BytecodeCompiler::visit(FieldDeref& exp) {
-    // get instructions to load the record 
+    // get instructions to load the record
     addInstructions(exp.base);
     // add the field to the names list
     int i = allocName(exp.field.name);
-    // compose instruction 
+    // compose instruction
     Instruction* instr = new Instruction(Operation::FieldLoad, optint_t(i));
     retFunc->instructions.push_back(*instr);
 }
 
 void BytecodeCompiler::visit(IndexExpr& exp) {
-    // load the record 
+    // load the record
     addInstructions(exp.base);
-    // eval the index 
+    // eval the index
     addInstructions(exp.index);
     // instruction
     Instruction* instr = new Instruction(Operation::IndexLoad, optint_t());
@@ -479,7 +479,7 @@ void BytecodeCompiler::visit(RecordExpr& exp) {
     retFunc->instructions.push_back(*alloc);
 
     for (std::map<Identifier*, Expression*>::iterator it = exp.record.begin(); it != exp.record.end(); it ++) {
-        // dup instruction 
+        // dup instruction
         Instruction* dup = new Instruction(Operation::Dup, noArg0);
         retFunc->instructions.push_back(*dup);
         // eval the value and add those instructions
@@ -487,7 +487,7 @@ void BytecodeCompiler::visit(RecordExpr& exp) {
         // add the name to the names array
         std::string field = it->first->name;
         int i = allocName(field);
-        // compose the instruction 
+        // compose the instruction
         Instruction* store = new Instruction(Operation::FieldStore, optint_t(i));
         retFunc->instructions.push_back(*store);
     }
@@ -497,7 +497,7 @@ void BytecodeCompiler::visit(Identifier& exp) {
     desc_t d = curTable->vars.at(exp.name);
     switch (d->type) {
         case GLOBAL: {
-            // use load_global 
+            // use load_global
             optint_t i = optint_t(d->index);
             Instruction* instr = new Instruction(Operation::LoadGlobal, i);
             retFunc->instructions.push_back(*instr);
@@ -510,8 +510,8 @@ void BytecodeCompiler::visit(Identifier& exp) {
             break;
         }
         case FREE: {
-            // recall d.index is an index into the free vars, so we have to 
-            // jump over local ref vars. 
+            // recall d.index is an index into the free vars, so we have to
+            // jump over local ref vars.
             optint_t i = optint_t(d->index + retFunc->local_reference_vars_.size());
             optint_t noArg0;
             Instruction* pushRefInstr = new Instruction(Operation::PushReference, i);
