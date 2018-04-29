a91dd0e8191b01809da70f4c291184739b3fbe3f
Anelise Newman <apnewman@mit.edu>
2018-04-29 12:13:03 -0400

    initial complete instr list and ir function skeleton

--- ir/ir.h
+++ ir/ir.h
@@ -8,145 +8,188 @@ struct IrFunc;
 
 typedef std::vector<IrInstruction> IrInstList;
 typedef std::experimental::optional<int32_t> optint_t;
+typedef std::vector<int32_t> TempList;
 typedef std::vector<IrFunc> IrProgram;
 
+
 enum class IrOp {
-   // TODO: finalize operation set
-   
    // Description: Move a constant from constants array into temp var
-   // Operand 1: index into the constants array of the constant to move
-   // Operand 2: N/A
-   // target: number of the temp to store the constant in 
-   // Result: target stores func.constants_[operand1]
+   // op0: index into the constants array of the constant to move
+   // temp0: number of the temp to store the constant in 
+   // Result: temp0 stores func.constants_[operand1]
    LoadConst,
 
    // Description: Load a function from functions list into temp 
-   // Operand 1: index into functions array to move
-   // Operand 2: N/A
-   // target: number of the temp to store the funtion in 
-   // Result: target stores the specified function
+   // op0: index into functions array to move
+   // temp0: number of the temp to store the funtion in 
+   // Result: temp0 stores the specified function
    LoadFunc,  
 
    // Description: Load a local into a temp
-   // Operand 1: index of the local to load
-   // Operand 2: N/A
-   // target: index of the temp to store into 
-   // Result: target stores the desired local 
+   // op0: index of the local to load
+   // temp0: index of the temp to store into 
+   // Result: temp0 stores the desired local 
    LoadLocal,  
 
    // Description: Store a value from a temp into a local 
-   // Operand 1: index of the temp containing the value to store
-   // Operand 2: index of the local to store into 
-   // target: N/A
+   // op0: index of the local to store into 
+   // temp0: index of the temp containing the value to store
    // Result: the local at index operand2 contains the value stored by operand1
    StoreLocal,  
 
    // Description: Load a global into a temp 
-   // Operand 1: index of the global to load
-   // Operand 2: N/A
-   // target: index of the temp to load into 
-   // Result: target contains the global var at index operand1
+   // op0: index of the global to load
+   // temp0: index of the temp to load into 
+   // Result: temp0 contains the global var at index operand1
    LoadGlobal,
 
    // Description: Store a value from a temp into a global
-   // Operand 1: index of the temp containing the value to store 
-   // Operand 2: index of the global to store into 
-   // target: N/A
+   // temp0: index of the temp containing the value to store 
+   // op0: index of the global to store into 
    // Result: the global at index operand2 contains the value stored by operand1
    StoreGlobal,
 
+   // Description: Allocate a record and store it to a temp 
+   // op0: N/A
+   // temp0: the index of the temp to store the record into
+   // Result: the temp0 contains a new empty record
+   AllocRecord,
+
+   // Description: Load a field from a record and store it into a temp
+   // op0: N/A
+   // temp0: the index of the temp to store the field value into 
+   // temp1: index of the temp containing the record to look in 
+   // temp2: index of the temp containing the field name
+   // Result: the temp0 contains the value of the field 
+   FieldLoad,
+  
+   // Description: Store a value into the field of a record
+   // op0: N/A
+   // temp0: the index of the temp containing the record to store into
+   // temp1: index of the temp containing the field name
+   // temp2: index of the temp containing the field value
+   // Result: the temp0 record now has the new field set 
+   FieldStore,
+
+   // Description: Load an index from a record and store it into a temp
+   // temp0: the index of the temp to store the index value into 
+   // temp1: index of the temp containing the record to look in 
+   // temp2: index of the temp containing the index
+   // Result: the temp0 contains the value of the index 
+   IndexLoad,
+  
+   // Description: Store a value into the index of a record
+   // temp0: the index of the temp containing the record to store into
+   // temp1: index of the temp containing the index
+   // temp2: index of the temp containing the index's new value
+   // Result: the temp0 record now has the new index set 
+   IndexStore,
+
+   // Description: Allocate a new closure 
+   // op0: Number of free vars passed to the closure
+   // temp0: index of temp containing the new closure
+   // temps 1-m: Contains references to the free vars we are going to pass
+   // Result: the temp0 now contains the new closure
+   AllocClosure,
+
+   // Description: call a closure
+   // op0: Number of args passed to the function 
+   // temp0: the index of the temp to contain the return val 
+   // temp1: index of temp storing the closure to call 
+   // temp 2-m: Contains values of the args we are passing
+   // Result: the temp0 contains the result of calling the function
+   Call, 
+
+   // Description: return from a function 
+   // temp0: index of temp containing return value
+   // Result: return from the function 
+   Return,
+   
    // Description: add the constants in two temps 
-   // Operand 1: temp index holding right value
-   // Operand 2: temp index holding left value
-   // target: number of the temp to store the value in 
-   // Result: target stores the result of doing operand1 + operand2
+   // temp0: number of the temp to store the value in 
+   // temp1: temp index holding right value
+   // temp2: temp index holding left value
+   // Result: temp0 stores the result of doing operand1 + operand2
    Add,
 
    // Description: performs arithmetic operation on two temps 
-   // Operand 1: temp index holding right value
-   // Operand 2: temp index holding left value
-   // target: number of the temp to store the value in 
-   // Result: target stores the result of doing op(operand2, operand1)
+   // temp0: number of the temp to store the value in 
+   // temp1: temp index holding right value
+   // temp2: temp index holding left value
+   // Result: temp0 stores the result of doing op(operand2, operand1)
    Sub, 
    Mul, 
    Div,
 
    // Description: computes unary minus
-   // Operand 1: value to negate
-   // Operand 2: N/A
-   // target: number of the temp to store the result in 
-   // Result: target stores -operand1
+   // temp0: number of the temp to store the result in 
+   // temp1: value to negate
+   // Result: temp0 stores -operand1
    Neg,
 
    // Description: computes a comparison on ints
-   // Operand 1: right value
-   // Operand 2: left value
-   // target: number of the temp to store the result in 
-   // Result: target stores bool indicating result of comparison
+   // temp1: right value
+   // temp2: left value
+   // temp0: number of the temp to store the result in 
+   // Result: temp0 stores bool indicating result of comparison
    Gt, 
    Geq,
 
    // Description: computes an equality between two vals (semantics from A2)
-   // Operand 1: right value
-   // Operand 2: left value
-   // target: number of the temp to store the result in 
-   // Result: target stores bool eq(operand2, operand1)
+   // temp1: right value
+   // temp2: left value
+   // temp0: number of the temp to store the result in 
+   // Result: temp0 stores bool eq(operand2, operand1)
    Eq, 
 
    // Description: computes a boolean operation (semantics from A2)
-   // Operand 1: right value
-   // Operand 2: left value
-   // target: number of the temp to store the result in
-   // Result: target stores bool op(operand1, operand2)
+   // temp1: right value
+   // temp2: left value
+   // temp0: number of the temp to store the result in
+   // Result: temp0 stores bool op(operand1, operand2)
    And, 
    Or,
 
    // Description: computes local negation
-   // Operand 1: value to negate
-   // Operand 2: N/A
-   // target: number of the temp to store the result in
-   // Result: target stores !operand1
+   // temp1: value to negate
+   // temp0: number of the temp to store the result in
+   // Result: temp0 stores !operand1
    Not,
 
    // Description: move to a given label 
-   // Operand 1: index of the label to jump to; should be unique 
-   // Operand 2: N/A
-   // target: N/A
+   // op0: index of the label to jump to; should be unique 
    // Result: execution transfers to the label
    Goto,
 
    // Description: move to a given label 
-   // Operand 1: index of the label to jump to; should be unique 
-   // Operand 2: N/A
-   // target: N/A
+   // op0: index of the label to jump to; should be unique 
    // Result: execution transfers to the label
    If,
 
    // Description: swap the values in two temps 
-   // Operand 1: index of one temp to swap
-   // Operand 2: index of the other temp to swap
-   // target: N/A
+   // temp0: index of one temp to swap
+   // temp1: index of the other temp to swap
    // Result: values in the temps are swapped 
    Swap,
 
    // Description: pops and discards the top of the stack
-   // Operand 1:   N/A
-   // Operand 2:   N/A
-   // target: N/A
    // Result: top element eliminated from stack
    Pop
 };
 
+
+// Each instruction contains an optional op0 and an array of temps storing its 
+// other operands. 
+// By convention, if the operation stores a value somewhere, it is put in 
+// temp 0.
 struct IrInstruction {
     IrOp operation; 
-    optint_t operand1;
-    optint_t operand2;
-    optint_t target;
-    IrInstruction(const IrOp operation, optint_t operand1, optint_t operand2, optint_t target) : 
+    optint_t op0;
+    TempList templist;
+    IrInstruction(const IrOp operation, optint_t op0, TempList templist):
         operation(operation), 
-        operand1(operand1), 
-        operand2(operand2), 
-        target(target) {};
+        op0(op0),
+        templist(templist) {}; 
 };
 
 struct IrFunc {
