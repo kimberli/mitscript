3fd8f1cd1c16be33bbc971a8f2deb17a8f9cf1c5
Anelise Newman <apnewman@mit.edu>
2018-04-29 13:57:47 -0400

    added machine code func for multiple args to ir folder

--- ir/ir_to_asm.cpp
+++ ir/ir_to_asm.cpp
@@ -6,12 +6,22 @@ IrInterpreter::IrInterpreter(vptr<IrProgram> irProgram) {
     curFunc = program->functions.at(0); 
     instructionIndex = 0;
     finished = false;
+
 }
 
 void IrInterpreter::run() {
+    // start the assembler on the function 
+    assm.start(asmFunc);
+
+    // translate to asm 
     while (!finished) {
         executeStep();
     }
+
+    // finish compiling 
+    assm.finish();
+    // call the asmFunc. Since this is main, presumably there are no args
+    asmFunc.call<Value*>();
 }
 
 void IrInterpreter::executeStep() {
--- ir/ir_to_asm.h
+++ ir/ir_to_asm.h
@@ -2,12 +2,16 @@
 
 #include "ir.h"
 #include "include/x64asm.h"
+#include "machine_code_func.cpp"
 
 using namespace std; 
 using namespace x64asm; 
 
 class IrInterpreter {
 private: 
+    x64asm::Assembler assm; 
+    x64asm::Function asmFunc;
+
     vptr<IrProgram> program;
     vptr<IrFunc> curFunc;
     int instructionIndex;
